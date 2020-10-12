230fd3fb2e4a16db902b9e8d4427dda138628188
tien trinh <nguusaongu@gmail.com>
2020-10-13 03:11:33 +0700

    c

--- src/app/core/sharing/sharing.module.ts
+++ src/app/core/sharing/sharing.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PipeModule } from '../pipe/pipe.module';
 import { ModalTrailerComponent } from '../component/modal-trailer/modal-trailer.component';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+import {UpdateUserComponent} from './components/update-user/update-user.component'
 @NgModule({
-  declarations: [ModalTrailerComponent],
+  declarations: [ModalTrailerComponent,UpdateUserComponent],
   imports: [
     CommonModule,
-    PipeModule
+    PipeModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
     ModalTrailerComponent
