5305e1d3c4b018934307d213a9c332b8005ff248
tien trinh <nguusaongu@gmail.com>
2020-10-13 22:00:43 +0700

    d

--- src/app/home/home-layout/movies/movies.module.ts
+++ src/app/home/home-layout/movies/movies.module.ts
@@ -6,14 +6,22 @@ import { ItemPhimComponent } from './item-phim/item-phim.component';
 import { MoviesComponent } from './movies.component';
 import { SlickCarouselModule } from 'ngx-slick-carousel';
 import { ItemPhimScComponent } from './item-phim-sc/item-phim-sc.component';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    
+        path: 'detail/:id',
+        component: MoviesComponent,
+        data: { title: 'Movie-detail' },
+  }]
 
 @NgModule({
   declarations: [PhimDangChieuComponent, PhimSapChieuComponent, ItemPhimComponent, MoviesComponent, ItemPhimScComponent],
   imports: [
     CommonModule,
     SlickCarouselModule,
+    RouterModule,
   ],
   exports: [
     PhimDangChieuComponent, PhimSapChieuComponent, ItemPhimComponent,MoviesComponent,ItemPhimScComponent
